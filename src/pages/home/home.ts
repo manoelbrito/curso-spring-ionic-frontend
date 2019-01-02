d31d0d79d4a2d2a79176bcf7692acbba109c4205
manoelbrito <manoelbrito05@gmail.com>
2019-01-02 17:12:09 -0400

    adcionando o header authorization cors

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -15,7 +16,9 @@ export class HomePage {
     senha: ""
   };
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  constructor(public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
 ionViewWillEnter() {
@@ -25,9 +28,17 @@ ionViewWillEnter() {
   this.menu.swipeEnable(true);
   }
 
-  login(){
+  login() {
     console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage');
+    
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        console.log(response.headers.get('Authorization'));
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});    
   }
 
 }
